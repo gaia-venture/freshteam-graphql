42a8a77acc2e1b4683cee03ccf5ab74119e8ca96
Tejas Dinkar <tejas@gja.in>
2022-09-11 06:41:05 +0530

    Added a somewhat working implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const {FreshteamGraphQLServer} = require("apollo-freshteam");
+const {ApolloServer} = require("apollo-server");
+const {apolloServerArgs} = require("apollo-freshteam");
 
 // The `listen` method launches a web server.
-FreshteamGraphQLServer.listen().then(({ url }) => {
+new ApolloServer(apolloServerArgs).listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}graphql`);
 });
--- packages/freshapi/graphql/graphql.js
+++ packages/freshapi/graphql/graphql.js
@@ -1,19 +1,72 @@
-const { FreshteamGraphQLServer } = require("apollo-freshteam");
+const { ApolloServerBase,  isHttpQueryError, runHttpQuery } = require('apollo-server-core');
+const { apolloServerArgs } = require("apollo-freshteam");
 
-async function main(args) {
-  const context = { req: { headers: args.__ow_headers } };
-  console.log(context);
-  console.log(args);
-  try {
-    const results = await FreshteamGraphQLServer.executeOperation({query: args.query, variables: args.variables}, context);
-    console.log(results);
-    return {"body": results}
-  } catch(e) {
-    return {
-      code: 500,
-      body: { errors: [e.message] }
-    };
+function buildRequest({__ow_method, __ow_body, __ow_headers, __ow_path, __ow_isBase64Encoded, ...body}) {
+  return {
+    method: __ow_method.toUpperCase(),
+    path: __ow_path,
+    headers: __ow_headers,
+    body: __ow_body
+            ? (__ow_isBase64Encoded ? Buffer.from(__ow_body, 'base64').toString() : __ow_body)
+            : body
   }
 }
 
-exports.main = main;
+class ApolloServer extends ApolloServerBase {
+  serverlessFramework() {
+    return true;
+  }
+
+  createGraphQLServerOptions(req) {
+    return super.graphQLServerOptions({ req });
+  }
+
+  createHandler() {
+    return async (args) => {
+      try {
+        const request = buildRequest(args);
+
+        if (request.method === 'POST' && !request.body) {
+          return {
+            body: 'POST body missing.',
+            status: 400,
+          };
+        }
+
+        await this.ensureStarted();
+        const options = await this.createGraphQLServerOptions(request);
+        const { graphqlResponse, responseInit } = await runHttpQuery(
+          [request],
+          {
+            method: request.method,
+            options: options,
+            query: request.body,
+            request: {
+              url: request.path,
+              method: request.method,
+              headers: new Headers(request.headers),
+            },
+          },
+          this.csrfPreventionRequestHeaders,
+        )
+        return {
+          body: graphqlResponse,
+          status: responseInit.status || 200,
+          headers: responseInit.headers,
+        }
+      } catch(error) {
+        if (isHttpQueryError(error)) {
+          return {
+            body: error.message,
+            status: error.statusCode,
+            headers: error.headers,
+          };
+        } else {
+          throw error;
+        }
+      }
+    }
+  }
+}
+
+exports.main = new ApolloServer(apolloServerArgs).createHandler();
